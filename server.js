cf4c8c47d05e325e13e89e724e68eec87b1d0220
Hosny Ahmed <ha18209900@gmail.com>
2022-06-02 21:43:10 +0200

    final resubmit

--- server.js
+++ server.js
@@ -1,36 +1,35 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
+
 // Require Express to run server and routes
 const express = require("express");
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
-const bodyParser = require("body-parser");
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require("cors");
 
 // Initialize the main project folder
 app.use(express.static("website"));
 
 // Setup Server
 
-app.get("/", (req, res) => {
-  // const data = req.body;
-  // console.log(data);
-});
-
 const port = 3000;
-app.listen(port, () => {
-  console.log("http://localhost:" + port);
+
+app.listen(port, (req, res) => {
+  console.log("server runing on http://localhost:" + port);
 });
 
 app.post("/", (req, res) => {
-  projectData.body = req.body;
+  projectData = req.body;
   console.log(projectData);
+});
+app.get("/all", (req, res) => {
+  res.send(projectData);
 });
--- website/app.js
+++ website/app.js
@@ -1,12 +1,12 @@
-/* Global Variables */
-const myapiKey = "ac1ff7ab7a50b6f1a59334c35bcc335c&units=imperial";
-// Create a new date instance dynamically with JS
+/*Global Variables */
+let myapiKey = "ac1ff7ab7a50b6f1a59334c35bcc335c&units=imperial";
 
+// Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + 1 + " / " + d.getDate() + " / " + d.getFullYear();
 
 const postData = async (url = "", data = {}) => {
-  console.log(data);
+  //   console.log(data);
   const response = await fetch(url, {
     method: "post",
     credentials: "same-origin",
@@ -21,27 +21,38 @@ const postData = async (url = "", data = {}) => {
   }
 };
 
-const updateUI = async () => {
+const callApi = async () => {
   let zipcode = document.getElementById("zip").value;
-  let mykey =
+  let apiUrl =
     "https://api.openweathermap.org/data/2.5/weather?zip=" +
     zipcode +
     "&appid=" +
     myapiKey;
-  const request = await fetch(mykey);
+  const getData = await fetch(apiUrl);
   try {
-    const allData = await request.json();
+    const response = await getData.json();
     let feeling = document.getElementById("feelings").value;
-    // let myDiv = document.createElement("div");
-    // myDiv.innerHTML = `you are feeling   ${feeling}  <br>
-    // ${newDate}`;
-    document.querySelector(".holder #date").innerHTML = newDate;
-    document.querySelector(".holder #temp").textContent =
-      Math.round(allData.main.temp) + " degrees";
-    document.querySelector(".holder #content").innerHTML = feeling;
-    postData("/", allData);
+    postData("/", {
+      temp: response.main.temp,
+      date: newDate,
+      feelings: feeling,
+    });
+    updateUi();
   } catch (error) {
     console.log("error", error);
   }
 };
-document.getElementById("generate").addEventListener("click", updateUI);
+
+const updateUi = async () => {
+  const request = await fetch("/all");
+  try {
+    const allData = await request.json();
+    document.getElementById("date").innerHTML = allData.date;
+    document.getElementById("temp").innerHTML = allData.temp;
+    document.getElementById("content").innerHTML =
+      "you are feeling " + allData.feelings;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+document.getElementById("generate").addEventListener("click", callApi);
